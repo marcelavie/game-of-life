dd047128436b9d69457c793d96244dfe00df3d7e
Marcela Lima <marcelalima.df@gmail.com>
2022-12-23 20:40:28 -0300

    feat: pequenas melhorias

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
 import time
 import pygame
-import numpy as np
+import numpy
 
-COLOR_BG = (10, 10, 10)
-COLOR_GRID = (40, 40, 40)
-COLOR_DIE_NEXT = (68, 4, 142)
-COLOR_ALIVE_NEXT = (216, 162, 247)
+cor_background = (10, 10, 10)
+cor_tabuleiro = (40, 40, 40)
+cor_cel_morta = (68, 4, 142)
+cor_cel_viva = (216, 162, 247)
 
 pygame.init()
 pygame.display.set_caption("joguinho da vida")
 
 
-def update(screen, cells, size, with_progress=False):
-    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
+def update(tela, cells, tamanho, progresso=False):
+    updated_cells = numpy.zeros((cells.shape[0], cells.shape[1]))
 
-    for row, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
-        color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+    for row, col in numpy.ndindex(cells.shape):
+        alive = numpy.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        color = cor_background if cells[row, col] == 0 else cor_cel_viva
 
         if cells[row, col] == 1:
             if alive < 2 or alive > 3:
-                if with_progress:
-                    color = COLOR_DIE_NEXT
+                if progresso:
+                    color = cor_cel_morta
             elif 2 <= alive <= 3:
                 updated_cells[row, col] = 1
-                if with_progress:
-                    color = COLOR_ALIVE_NEXT
+                if progresso:
+                    color = cor_cel_viva
         else:
             if alive == 3:
                 updated_cells[row, col] = 1
-                if with_progress:
-                    color = COLOR_ALIVE_NEXT
+                if progresso:
+                    color = cor_cel_viva
 
-        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
+        pygame.draw.rect(tela, color, (col * tamanho, row * tamanho, tamanho - 1, tamanho - 1))
 
     return updated_cells
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    tela = pygame.display.set_mode((800, 600))
 
-    cells = np.zeros((60, 80))
-    screen.fill(COLOR_GRID)
-    update(screen, cells, 10)
+    cells = numpy.zeros((60, 80))
+    tela.fill(cor_tabuleiro)
+    update(tela, cells, 10)
 
     pygame.display.flip()
     pygame.display.update()
@@ -51,25 +51,25 @@ def main():
     running = False
 
     while True:
-        for Q in pygame.event.get():
-            if Q.type == pygame.QUIT:
+        for saida in pygame.event.get():
+            if saida.type == pygame.QUIT:
                 pygame.quit()
                 return
-            elif Q.type == pygame.KEYDOWN:
-                if Q.key == pygame.K_SPACE:
+            elif saida.type == pygame.KEYDOWN:
+                if saida.key == pygame.K_SPACE:
                     running = not running
-                    update(screen, cells, 10)
+                    update(tela, cells, 10)
                     pygame.display.update()
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cells[pos[1] // 10, pos[0] // 10] = 1
-                update(screen, cells, 10)
+                update(tela, cells, 10)
                 pygame.display.update()
 
-        screen.fill(COLOR_GRID)
+        tela.fill(cor_tabuleiro)
 
         if running:
-            cells = update(screen, cells, 10, with_progress=True)
+            cells = update(tela, cells, 10, progresso=True)
             pygame.display.update()
 
         time.sleep(0.001)
