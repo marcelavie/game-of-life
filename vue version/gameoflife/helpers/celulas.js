c7ac97365bce6455acbc6ac91952b14e80866200
Marcela Lima <marcelalima.df@gmail.com>
2022-12-26 10:33:35 -0300

    recomeçando a lógica

--- vue version/gameoflife/helpers/celulas.js
+++ vue version/gameoflife/helpers/celulas.js
@@ -1,11 +1,2 @@
-/**
- *
- *
- * @param {number} valor
- * @param {number} min
- * @param {number} max
- */
-export const Intervalo = (valor, min, max) => {
-    return !(valor < min || valor > max)
-  }
+//  lógica do 0
   
